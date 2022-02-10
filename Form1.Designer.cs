865890376b5677a03ed68a9625707ecf403fbd1f
ranadheer.k.2451@gmail.com <ranadheer.k.2451@gmail.com>
2022-02-10 17:55:51 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProduct = new System.Windows.Forms.DataGridView();
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.Label();
+            this.txtNames = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.Label();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProduct
+            // 
+            this.dgvProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProduct.Location = new System.Drawing.Point(424, 93);
+            this.dgvProduct.Name = "dgvProduct";
+            this.dgvProduct.RowHeadersWidth = 62;
+            this.dgvProduct.RowTemplate.Height = 28;
+            this.dgvProduct.Size = new System.Drawing.Size(588, 375);
+            this.dgvProduct.TabIndex = 0;
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(697, 493);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(88, 38);
+            this.btnFetch.TabIndex = 1;
+            this.btnFetch.Text = "Fetch";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(256, 149);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 26);
+            this.txtId.TabIndex = 3;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(256, 415);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 41);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.AutoSize = true;
+            this.txtName.Location = new System.Drawing.Point(123, 226);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(94, 20);
+            this.txtName.TabIndex = 5;
+            this.txtName.Text = "Enter Name";
+            // 
+            // txtNames
+            // 
+            this.txtNames.Location = new System.Drawing.Point(256, 226);
+            this.txtNames.Name = "txtNames";
+            this.txtNames.Size = new System.Drawing.Size(100, 26);
+            this.txtNames.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.AutoSize = true;
+            this.txtPrice.Location = new System.Drawing.Point(123, 308);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(87, 20);
+            this.txtPrice.TabIndex = 7;
+            this.txtPrice.Text = "Enter Price";
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(256, 302);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(100, 26);
+            this.txtCost.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 603);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtNames);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFetch);
+            this.Controls.Add(this.dgvProduct);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProduct;
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label txtName;
+        private System.Windows.Forms.TextBox txtNames;
+        private System.Windows.Forms.Label txtPrice;
+        private System.Windows.Forms.TextBox txtCost;
+    }
+}
+
